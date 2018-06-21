e1de4a86c4d98977183baa50b37b1a787db710a4
Fred Hornsey <fred@hornsey.us>
2018-06-21 01:44:02 -0500

    Added Titles

--- ck2_new_db.sql
+++ ck2_new_db.sql
@@ -4,57 +4,62 @@
  */
 
 /* Create or Override Database */
-DROP DATABASE IF EXISTS ck2;
-CREATE DATABASE IF NOT EXISTS ck2;
-USE ck2;
+drop database if exists ck2;
+create database if not exists ck2;
+use ck2;
+
+/* User =================================================================== */
+create or replace user 'ck2' identified by 'ck2password';
+grant all privileges on `ck2`.* to 'ck2'@localhost;
+
 
 /* Basic Infomation ======================================================= */
 
 /* Snapshots */
-CREATE TABLE Snapshots (date CHAR(10) KEY);
+create table Snapshots (date char(10) key);
 
 /* Cultures */
-CREATE TABLE Cultures (
-    id VARCHAR(100) KEY,
-    name VARCHAR(100) NOT NULL
+create table Cultures (
+    id varchar(100) key,
+    name varchar(100) not null
 );
 
 /* Religion */
-CREATE TABLE Religions (
-    id VARCHAR(100) KEY,
-    name VARCHAR(100) NOT NULL
+create table Religions (
+    id varchar(100) key,
+    name varchar(100) not null
 );
 
 /* Character Information ================================================== */
 
 /* Dynasties */
-CREATE TABLE Dynasties (
-    id INT UNSIGNED KEY,
-    name VARCHAR(100) NOT NULL
+create table Dynasties (
+    id int unsigned key,
+    name varchar(100) not null
 );
 
 /* Characters */
 
-CREATE TABLE Characters (
-    id INT UNSIGNED KEY,
-    birth CHAR(10) NOT NULL,
-    death CHAR(10),
-    is_female BOOLEAN NOT NULL,
+create table Characters (
+    id int unsigned key,
+    birth char(10) not null,
+    death char(10),
+    is_female boolean not null,
 
-    mother_id INT UNSIGNED check (mother_id != id),
-    FOREIGN KEY (mother_id)
-        REFERENCES Characters(id)
-        ON DELETE CASCADE,
+    mother_id int unsigned check (mother_id != id),
+    foreign key (mother_id)
+        references Characters(id)
+        on delete cascade,
 
-    real_father_id INT UNSIGNED CHECK(real_father_id != id),
-    FOREIGN KEY (real_father_id)
-        REFERENCES Characters(id)
-        ON DELETE CASCADE
+    real_father_id int unsigned check(real_father_id != id),
+    foreign key (real_father_id)
+        references Characters(id)
+        on delete cascade
 );
 
-/* Enforce the fact the mothers are female if a character has one */
+/* Enforce the fact a mothers is female if a character has one */
 create function is_female(id int unsigned) returns boolean return id is null or (
-(select count(c.id) from Characters as c where c.id = id and c.is_female = true) = 1);
+    (select count(c.id) from Characters as c where c.id = id and c.is_female = true) = 1);
 delimiter $$
 create trigger mother_is_female before insert on Characters for each row begin
     if not is_female(new.mother_id) then
@@ -63,9 +68,9 @@ create trigger mother_is_female before insert on Characters for each row begin
     end; $$
 delimiter ;
 
-/* Enforce the fact the fathers are male if a character has one */
+/* Enforce the fact a fathers is male if a character has one */
 create function is_male(id int unsigned) returns boolean return id is null or (
-(select count(c.id) from Characters as c where c.id = id and c.is_female = false) = 1);
+    (select count(c.id) from Characters as c where c.id = id and c.is_female = false) = 1);
 delimiter $$
 create trigger father_is_male before insert on Characters for each row begin
     if not is_male(new.real_father_id) then
@@ -75,76 +80,249 @@ create trigger father_is_male before insert on Characters for each row begin
 delimiter ;
 
 /* Character_Status */
-CREATE TABLE Character_Status (
-    character_id INT UNSIGNED,
-    FOREIGN KEY (character_id)
-        REFERENCES Characters (id)
-        ON DELETE CASCADE,
-    date CHAR(10),
-    FOREIGN KEY (date)
-        REFERENCES Snapshots(date)
-        ON DELETE CASCADE,
-    KEY (character_id, date),
+create table Character_Status (
+    character_id int unsigned,
+    foreign key (character_id)
+        references Characters (id)
+        on delete cascade,
+    date char(10),
+    foreign key (date)
+        references Snapshots(date)
+        on delete cascade,
+    key (character_id, date),
 
-    name VARCHAR(100) NOT NULL,
-    health FLOAT NOT NULL,
-    wealth FLOAT NOT NULL,
-    piety FLOAT NOT NULL,
-    prestige FLOAT NOT NULL,
-    diplomacy TINYINT UNSIGNED NOT NULL,
-    martial TINYINT UNSIGNED NOT NULL,
-    stewardship TINYINT UNSIGNED NOT NULL,
-    intrigue TINYINT UNSIGNED NOT NULL,
-    learning TINYINT UNSIGNED NOT NULL,
+    name varchar(100) not null,
+    health float not null,
+    wealth float not null,
+    piety float not null,
+    prestige float not null,
+    diplomacy tinyint unsigned not null,
+    martial tinyint unsigned not null,
+    stewardship tinyint unsigned not null,
+    intrigue tinyint unsigned not null,
+    learning tinyint unsigned not null,
 
-    dynasty_id INT UNSIGNED NOT NULL,
-    FOREIGN KEY (dynasty_id)
-        REFERENCES Dynasties(id)
-        ON DELETE CASCADE,
+    dynasty_id int unsigned not null,
+    foreign key (dynasty_id)
+        references Dynasties(id)
+        on delete cascade,
 
-    culture_id VARCHAR(100) NOT NULL,
-    FOREIGN KEY (culture_id)
-        REFERENCES Cultures(id)
-        ON DELETE CASCADE,
+    culture_id varchar(100) not null,
+    foreign key (culture_id)
+        references Cultures(id)
+        on delete cascade,
 
-    religion_id VARCHAR(100) NOT NULL,
-    FOREIGN KEY (religion_id)
-        REFERENCES Religions(id)
-        ON DELETE CASCADE,
+    religion_id varchar(100) not null,
+    foreign key (religion_id)
+        references Religions(id)
+        on delete cascade,
 
-    perceived_father_id INT UNSIGNED,
-    FOREIGN KEY (perceived_father_id)
-        REFERENCES Characters(id)
-        ON DELETE CASCADE,
+    perceived_father_id int unsigned check (character_id != perceived_father_id),
+    foreign key (perceived_father_id)
+        references Characters(id)
+        on delete cascade,
 
-    spouse_id INT UNSIGNED,
-    FOREIGN KEY (spouse_id)
-        REFERENCES Characters(id)
-        ON DELETE CASCADE
+    spouse_id int unsigned check (character_id != spouse_id),
+    foreign key (spouse_id)
+        references Characters(id)
+        on delete cascade
 );
+
+/* Enforce the fact a perceived fathers is male if a character has one */
+delimiter $$
+create trigger perceived_father_is_male before insert on Character_Status for each row begin
+    if not is_male(new.perceived_father_id) then
+        signal sqlstate '45000' set message_text = 'Perceived father is not male';
+    end if;
+    end; $$
+delimiter ;
+
+/* Enforce the fact a spouse is of the opposite sex if a character has one */
+create function is_opposite_sex(id int unsigned, this_sex boolean) returns boolean return
+    id is null or ((select count(c.id) from Characters as c where c.id = id and c.is_female != this_sex) = 1);
+
+delimiter $$
+create trigger spouse_is_opposite_sex before insert on Character_Status for each row begin
+    if not is_opposite_sex(new.spouse_id, is_female(new.character_id)) then
+        signal sqlstate '45000' set message_text = 'Marriage is Same Sex! O:';
+    end if;
+    end; $$
+delimiter ;
 
 /* Traits */
-CREATE TABLE Traits (
-    id INT UNSIGNED KEY,
-    name VARCHAR(100) NOT NULL
+create table Traits (
+    id int unsigned key,
+    name varchar(100) not null
 );
-CREATE TABLE Character_Traits (
-    character_id INT UNSIGNED,
-    date CHAR(10),
-    FOREIGN KEY (character_id, date)
-        REFERENCES Character_Status (character_id, date)
-        ON DELETE CASCADE,
-    trait_id INT UNSIGNED,
-    FOREIGN KEY (trait_id)
-        REFERENCES Traits(id)
-        ON DELETE CASCADE,
-    KEY(character_id, date, trait_id)
+create table Character_Traits (
+    character_id int unsigned,
+    date char(10),
+    foreign key (character_id, date)
+        references Character_Status (character_id, date)
+        on delete cascade,
+    trait_id int unsigned,
+    foreign key (trait_id)
+        references Traits(id)
+        on delete cascade,
+    key(character_id, date, trait_id)
 );
 
-/* User =================================================================== */
-create or replace user 'ck2' identified by 'ck2password';
-grant all privileges on `ck2`.* to 'ck2'@localhost;
+/* Title Information =================================================================== */
+
+create table Titles (
+    id varchar(100) key
+);
+
+create table Title_Status (
+    title_id varchar(100),
+    holder_id int unsigned,
+    date char(10),
+    foreign key (holder_id, date)
+        references Character_Status (character_id, date)
+        on delete cascade,
+    key(title_id, date),
+    name varchar(10)
+);
+
+create table Empires (
+    title_id varchar(100),
+    foreign key (title_id)
+        references Titles(id)
+        on delete cascade
+);
+
+create table Empire_Status (
+    title_id varchar(100),
+    date char(10),
+    foreign key (title_id, date)
+        references Title_Status (title_id, date)
+        on delete cascade,
+    foreign key (title_id)
+        references Empires (title_id)
+        on delete cascade
+);
+
+create table Kingdoms (
+    title_id varchar(100),
+    foreign key (title_id)
+        references Titles(id)
+        on delete cascade
+);
+
+create table Kingdom_Status (
+    title_id varchar(100),
+    date char(10),
+    foreign key (title_id, date)
+        references Title_Status (title_id, date)
+        on delete cascade,
+    foreign key (title_id)
+        references Kingdoms (title_id)
+        on delete cascade,
+    de_jure_empire_id varchar(100),
+    foreign key (de_jure_empire_id)
+        references Empires (title_id)
+        on delete cascade,
+    de_facto_empire_id varchar(100),
+    foreign key (de_facto_empire_id)
+        references Empires (title_id)
+        on delete cascade
+);
+
+create table Duchies (
+    title_id varchar(100),
+    foreign key (title_id)
+        references Titles(id)
+        on delete cascade
+);
+
+create table Duchy_Status (
+    title_id varchar(100),
+    date char(10),
+    foreign key (title_id, date)
+        references Title_Status (title_id, date)
+        on delete cascade,
+    foreign key (title_id)
+        references Duchies (title_id)
+        on delete cascade,
+    de_jure_kingdom_id varchar(100),
+    foreign key (de_jure_kingdom_id)
+        references Kingdoms (title_id)
+        on delete cascade,
+    de_facto_kingdom_id varchar(100),
+    foreign key (de_facto_kingdom_id)
+        references Kingdoms (title_id)
+        on delete cascade
+);
+
+create table Counties (
+    title_id varchar(100),
+    foreign key (title_id)
+        references Titles(id)
+        on delete cascade,
+    duchy_id varchar(100),
+    foreign key (duchy_id)
+        references Duchies(title_id)
+        on delete cascade,
+    independent boolean not null
+);
+
+create table County_Status (
+    title_id varchar(100),
+    date char(10),
+    foreign key (title_id, date)
+        references Title_Status (title_id, date)
+        on delete cascade,
+    foreign key (title_id)
+        references Counties (title_id)
+        on delete cascade,
+
+    culture_id varchar(100) not null,
+    foreign key (culture_id)
+        references Cultures(id)
+        on delete cascade,
+    religion_id varchar(100) not null,
+    foreign key (religion_id)
+        references Religions(id)
+        on delete cascade
+);
+
+create table Baronies (
+    title_id varchar(100),
+    foreign key (title_id)
+        references Titles(id)
+        on delete cascade,
+    county_id varchar(100),
+    foreign key (county_id)
+        references Counties(title_id)
+        on delete cascade
+);
+
+create table Barony_Status (
+    title_id varchar(100),
+    date char(10),
+    foreign key (title_id, date)
+        references Title_Status (title_id, date)
+        on delete cascade,
+    foreign key (title_id)
+        references Baronies (title_id)
+        on delete cascade
+);
+
+/* Test Data =================================================================== */
 
 insert into Characters values (1, "0000-00-00", null, true, null, null);
 insert into Characters values (2, "0000-00-00", null, false, null, null);
 insert into Characters values (3, "0000-00-00", null, false, 1, 2);
+insert into Characters values (4, "0000-00-00", null, true, null, null);
+
+insert into Dynasties values (1, "a Dynasty");
+insert into Cultures values ("a", "a Culture");
+insert into Religions values ("a", "a Religion");
+
+insert into Snapshots values ("0000-00-00");
+
+insert into Character_Status value (1, "0000-00-00", "Char1", 0.0,0.0,0.0,0.0, 0,0,0,0,0, 1, "a","a", null, 2);
+insert into Character_Status value (2, "0000-00-00", "Char2", 0.0,0.0,0.0,0.0, 0,0,0,0,0, 1, "a","a", null, 1);
+insert into Character_Status value (3, "0000-00-00", "Char3", 0.0,0.0,0.0,0.0, 0,0,0,0,0, 1, "a","a", 2, 4);
+insert into Character_Status value (4, "0000-00-00", "Char4", 0.0,0.0,0.0,0.0, 0,0,0,0,0, 1, "a","a", null, 3);
+
