688e7d4ddb76f8fa9ef6394be311d847415ba563
Fred Hornsey <fred@hornsey.us>
2018-06-29 17:09:49 -0500

    Build Database script

---
+++ build_database.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+
+if [ $# -ne 1 ]
+then
+    echo "arguments: JSON_SRC" 1>&2
+    exit 1
+fi
+
+./ck2db.py gamedata
+for i in $1/*.json
+do
+    echo "$i =================================================="
+    ./ck2db savefile "$i"
+done
+echo "DONE BUILDING DATABASE"
+
