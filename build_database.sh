99b58cab6b444693569120bcd07c37e2fb5b2bba
Fred Hornsey <fred@hornsey.us>
2018-06-29 18:28:37 -0500

    Filter by year

--- build_database.sh
+++ build_database.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
 set -e
 
-if [ $# -ne 1 ]
+if [ $# -ne 3 ]
 then
-    echo "arguments: JSON_SRC" 1>&2
+    echo "arguments: JSON_SRC START_YEAR END_YEAR" 1>&2
     exit 1
 fi
 
-./ck2db.py gamedata
+start_year=$(printf '%.4d' $(expr "$2" '-' 1))
+end_year=$(printf '%.4d' "$3")
+
+#./ck2db.py gamedata
 for i in $1/*.json
 do
-    echo "$i =================================================="
-    ./ck2db.py savefile "$i"
+    year=$(basename -s '.json' $i)
+    if [ "$year" \> "$start_year~" -a "$year" \< "${end_year}~" ]
+    then
+        echo "$i =================================================="
+        #./ck2db.py savefile "$i"
+    fi
 done
 echo "DONE BUILDING DATABASE"
 
